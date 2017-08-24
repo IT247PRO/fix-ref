086312539a20aea669b341a12d88a18c0686c9fb
Pranav K <prkrishn@hotmail.com>
2017-08-24 09:47:02 -0700

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace fix_ref.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
